041001e1687fbaeb9817fb0a9350b2fc9eb6d917
iPlowPlow <plowplow2011@hotmail.fr>
2018-03-07 16:51:47 +0100

    push

--- MachineLearningTP1/source.cpp
+++ MachineLearningTP1/source.cpp
@@ -1,20 +1,47 @@
 #include <stdlib.h>
+#include <iostream>
+#include <time.h>
 
 extern "C"
 {
-	_declspec(dllexport) int add_to_42(int value_to_add)
+	double randDouble(double low, double high)
 	{
-		return value_to_add + 42;
+		double temp;
+
+		/* swap low & high around if the user makes no sense */
+		if (low > high)
+		{
+			temp = low;
+			low = high;
+			high = temp;
+		}
+
+		/* calculate the random number & return it */
+		temp = (rand() / (static_cast<double>(RAND_MAX) + 1.0))
+			* (high - low) + low;
+		return temp;
+	}
+
+	int calculateSign(double* W, double x, double y) {
+		double sign = W[1] * x + W[2] * y + W[0];
+		if (sign < 0) return -1;
+		return 1;
 	}
 
 	_declspec(dllexport) double* linear_create() {
-		double W[3];
-		//W = new double[2 + 1];
-		W[0] = rand() % 1 - 1;
-		W[1] = rand() % 1 - 1;
-		W[2] = rand() % 1 - 1;
+		srand(time(NULL));
+		double *W = (double*) malloc(sizeof(double) * 3);
+		W[0] = randDouble(0.0, 2.0) -1;
+		W[1] = randDouble(0.0, 2.0) - 1;
+		W[2] = randDouble(0.0, 2.0) - 1;
+
 		return W;
 	}
+
+	_declspec(dllexport) void linear_delete(double* W) {
+		free(W);
+	}
+
 	//elem = nombre elem ; elem size = taille elem
 	_declspec(dllexport) void linear_train_classification(double* W, int elem, int elemsize, double* tabSphere) {
 		double alpha = 0.1;
@@ -24,7 +51,7 @@ extern "C"
 				double x = tabSphere[i];
 				double y = tabSphere[i + 1];
 				double sign = calculateSign(W, x, y);
-	
+					
 				W[0] = W[0] + alpha * (tabSphere[i + 2] - sign);
 				W[1] = W[1] + alpha * (tabSphere[i + 2] - sign)*x;
 				W[2] = W[2] + alpha * (tabSphere[i + 2] - sign)*y;
@@ -33,11 +60,8 @@ extern "C"
 		}	
 	}
 
-	int calculateSign(double* W , double x, double y) {
-		double sign = W[1] * x + W[2] * y + W[0];
-		if (sign < 0) return -1;
-		return 1;
+	_declspec(dllexport) double linear_classify(double* W, double x, double y) {
+		return calculateSign(W, x, y);
 	}
-
-
+	
 }
